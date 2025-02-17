f73bde1bf0f2566c2637477d79d23e0a50d0f0d0
LasseMempel <lassemempel@gmail.com>
2025-02-17 22:58:16 +0100

    updated baseURI

--- navisOne/navisOne.py
+++ navisOne/navisOne.py
@@ -8,17 +8,19 @@ df = pd.read_csv("items.csv")
 parentDf = pd.read_csv("parent.csv")
 #print(df)
 
-baseUri = "https://www.lassemempel.github.io/Terminologien/NAVISone/"
+baseUri = "https://www.lassemempel.github.io/Terminologien/NAVISone"
+thesaurus = URIRef(baseUri)
+thesaurusAddendum = URIRef(baseUri + "/")
 
 parentDfColumns = ["id","navisid","de","en","dk","nl","fr","it","es","pl","gr","he"]
 
 dfColumns = ["id","navisid","fk_id_parent","de","en","es","it","nl","dk","gr","fr","pl","he","desc_en","desc_de","origindesc","gettyaat","gettyaatrelationtype","wikidata","wikidatarelationtype"]
 
 g = Graph()
-g.add((URIRef(baseUri), RDF.type, SKOS.ConceptScheme))
-g.add((URIRef(baseUri), DC.title, Literal("NAVISone")))
-g.add((URIRef(baseUri), DC.description, Literal("NAVISone ist ein Thesaurus über Schiffsbegriffe", lang="de")))
-g.add((URIRef(baseUri), DC.creator, Literal("Florian Thiery")))
+g.add((thesaurusAddendum, RDF.type, SKOS.ConceptScheme))
+g.add((thesaurusAddendum, DC.title, Literal("NAVISone")))
+g.add((thesaurusAddendum, DC.description, Literal("NAVISone ist ein Thesaurus über Schiffsbegriffe", lang="de")))
+g.add((thesaurusAddendum, DC.creator, Literal("Florian Thiery")))
 
 for index, row in parentDf.iterrows():
     concept = URIRef(baseUri + str(row["id"]))
@@ -26,10 +28,10 @@ for index, row in parentDf.iterrows():
     for language in parentDfColumns[2:]:
         if not pd.isnull(row[language]):
             g.add((concept, SKOS.prefLabel, Literal(row[language], lang=language)))
-    g.add((concept, SKOS.inScheme, URIRef(baseUri)))
+    g.add((concept, SKOS.inScheme, thesaurusAddendum))
     # top concept
-    g.add((concept, SKOS.topConceptOf, URIRef(baseUri)))
-    g.add((URIRef(baseUri), SKOS.hasTopConcept, concept))
+    g.add((concept, SKOS.topConceptOf, thesaurusAddendum))
+    g.add((thesaurusAddendum, SKOS.hasTopConcept, concept))
     # iterate over all rows in df where fk_id_parent == id
     for index2, row2 in df[df["fk_id_parent"] == row["id"]].iterrows():
         concept2 = URIRef(baseUri + str(row2["id"]))
@@ -37,7 +39,7 @@ for index, row in parentDf.iterrows():
         for language in dfColumns[3:12]:
             if not pd.isnull(row2[language]):
                 g.add((concept2, SKOS.prefLabel, Literal(row2[language], lang=language)))
-        g.add((concept2, SKOS.inScheme, URIRef(baseUri)))
+        g.add((concept2, SKOS.inScheme, thesaurusAddendum))
         g.add((concept, SKOS.narrower, concept2))
         g.add((concept2, SKOS.broader, concept))
         # add relations
